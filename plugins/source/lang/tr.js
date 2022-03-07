ad31cbbd42aca238f06fd58da5c061fa2674d9be
Müslüm Barış Korkmazer <11800544+babico@users.noreply.github.com>
2022-03-07 22:27:09 +0300

    Update tr.js

--- plugins/source/lang/tr.js
+++ plugins/source/lang/tr.js
@@ -6,7 +6,7 @@
  * Author: 
  */
 
- theUILang.getSource		= "Get .torrent";
- theUILang.cantFindTorrent	= "Source torrent file for this download not found.";
+ theUILang.getSource		= ".torrent Al";
+ theUILang.cantFindTorrent	= "Bu indirme için kaynak .torrent dosyası bulunamadı.";
 
 thePlugins.get("source").langLoaded();
